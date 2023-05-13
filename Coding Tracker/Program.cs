a9b3c1fc4857e9c45902a6a8e9e6433ed8329ae0
hgoel <hgoel6600@gmail.com>
2023-05-13 11:06:23 +0530

    keep one messge

--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,10 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#! modification"); Console.WriteLine("Hello, C#! modification");
-Console.WriteLine("Hello, C#! modification");
-Console.WriteLine("Hello, C#! modification");
-Console.WriteLine("Hello, C#! modification");
-Console.WriteLine("Hello, C#! modification");
-Console.WriteLine("Hello, C#! modification");
-Console.WriteLine("Hello, C#! modification");
-Console.WriteLine("Hello, C#! modification");
-
+Console.WriteLine("Hello, C#! modification"); 
