075a49c0f73a3087fd908dd940a8f047ea3c3481
himanshu goel <himanshu.kiet@hotmail.com>
2023-05-13 11:10:22 +0530

    Revert "keep one messge"
    This reverts commit a9b3c1fc4857e9c45902a6a8e9e6433ed8329ae0.

--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#! modification"); 
+Console.WriteLine("Hello, C#! modification"); Console.WriteLine("Hello, C#! modification");
+Console.WriteLine("Hello, C#! modification");
+Console.WriteLine("Hello, C#! modification");
+Console.WriteLine("Hello, C#! modification");
+Console.WriteLine("Hello, C#! modification");
+Console.WriteLine("Hello, C#! modification");
+Console.WriteLine("Hello, C#! modification");
+Console.WriteLine("Hello, C#! modification");
+
